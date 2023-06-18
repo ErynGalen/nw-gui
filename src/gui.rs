a47612a531d8604d19283a5508973ea7241d7245
Paul Mirkwood <paul.mirkwood@protonmail.com>
2023-06-18 19:27:06 +0200

    new architecture

--- src/gui.rs
+++ src/gui.rs
@@ -7,24 +7,24 @@ use embedded_graphics::{
     text::Text,
 };
 
-use crate::calculator::{Color, DeviceDislay};
+use crate::calculator::{Color, DeviceDislay, Event, KeyCode};
+
+pub mod widgets;
 
 pub use embedded_graphics::mono_font::ascii::FONT_7X13 as NORMAL_FONT;
 
-#[derive(Debug)]
-pub enum Widget {
-    Text(TextWidget),
-    Rect(RectWidget),
-}
-impl Widget {
-    pub fn render(&self, target: &mut DeviceDislay) {
-        match self {
-            Self::Text(w) => w.render(target),
-            Self::Rect(w) => w.render(target),
-        }
-    }
+pub trait WidgetCollection {
+    type Item: Widget;
+
+    fn len(&self) -> usize;
+    fn get(&self, n: usize) -> Option<&Self::Item>;
+    fn get_mut(&mut self, n: usize) -> Option<&mut Self::Item>;
 }
 
+pub trait Widget {
+    fn render(&self, target: &mut DeviceDislay);
+    fn on_event(&mut self, e: Event);
+}
 #[derive(Debug)]
 pub struct TextWidget {
     pub bounding_box: Rectangle,
@@ -74,9 +74,9 @@ pub struct RectWidget {
     /// top, right, bottom, left
     pub border_width: u32,
 }
-
-impl RectWidget {
-    pub fn render(&self, target: &mut DeviceDislay) {
+impl Widget for RectWidget {
+    fn on_event(&mut self, _e: Event) {}
+    fn render(&self, target: &mut DeviceDislay) {
         let style = PrimitiveStyleBuilder::new()
             .fill_color(self.fill_color)
             .stroke_color(self.border_color)
@@ -86,36 +86,71 @@ impl RectWidget {
     }
 }
 
-pub struct Splitter {
-    pub bounding_box: Rectangle,
-    pub vertical: bool,
-    pub ratio: f32,
+#[derive(Debug, Clone, Copy)]
+pub struct Grid<const X: usize, const Y: usize, C: WidgetCollection> {
+    bounding_box: Rectangle,
+    grid: [[Option<usize>; Y]; X],
+    selected: (usize, usize),
+    children: C,
 }
-
-impl Splitter {
-    pub fn bounding_box_a(&self) -> Rectangle {
-        let mut size = self.bounding_box.size;
-        if self.vertical {
-            let height = self.ratio * size.height as f32;
-            size = Size::new(size.width, height as u32);
-        } else {
-            let width = self.ratio * size.width as f32;
-            size = Size::new(width as u32, size.height);
+impl<const X: usize, const Y: usize, C: WidgetCollection> Grid<X, Y, C> {
+    pub fn new(bounding_box: Rectangle, children: C) -> Self {
+        Self {
+            bounding_box,
+            grid: [[None; Y]; X],
+            selected: (0, 0),
+            children,
         }
-        Rectangle::new(self.bounding_box.top_left, size)
     }
-    pub fn bounding_box_b(&self) -> Rectangle {
-        let mut size = self.bounding_box.size;
-        let mut top_left = self.bounding_box.top_left;
-        if self.vertical {
-            let removed_height = self.ratio * size.height as f32;
-            size = Size::new(size.width, size.height - removed_height as u32);
-            top_left = Point::new(top_left.x, top_left.y + removed_height as i32);
-        } else {
-            let removed_width = self.ratio * size.width as f32;
-            size = Size::new(size.width - removed_width as u32, size.height);
-            top_left = Point::new(top_left.x + removed_width as i32, top_left.y);
+}
+impl<'a, const X: usize, const Y: usize, C: WidgetCollection> Widget for Grid<X, Y, C> {
+    fn render(&self, target: &mut DeviceDislay) {
+        for n in 0..self.children.len() {
+            self.children.get(n).unwrap().render(target);
         }
-        Rectangle::new(top_left, size)
+    }
+    fn on_event(&mut self, e: Event) {
+        match e {
+            Event::KeyDown(key) => {
+                let mut new_selected = self.selected;
+                match key {
+                    KeyCode::Left => {
+                        if new_selected.0 > 0 {
+                            new_selected.0 -= 1;
+                        }
+                    }
+                    KeyCode::Right => {
+                        if new_selected.0 < X {
+                            new_selected.0 += 1;
+                        }
+                    }
+                    KeyCode::Up => {
+                        if new_selected.1 > 0 {
+                            new_selected.1 -= 1;
+                        }
+                    }
+                    KeyCode::Down => {
+                        if new_selected.1 < Y {
+                            new_selected.1 += 1;
+                        }
+                    }
+                    _ => {
+                        let selected_child = self.grid[self.selected.0][self.selected.1].unwrap();
+                        if let Some(selected_child) = self.children.get_mut(selected_child) {
+                            selected_child.on_event(e);
+                        }
+                    }
+                }
+                if self.grid[new_selected.0][new_selected.1].is_some() {
+                    self.selected = new_selected;
+                }
+            }
+            _ => {
+                let selected_child = self.grid[self.selected.0][self.selected.1].unwrap();
+                if let Some(selected_child) = self.children.get_mut(selected_child) {
+                    selected_child.on_event(e);
+                }
+            }
+        }
     }
 }
